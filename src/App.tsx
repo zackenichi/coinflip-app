0a276472a7de8abb1f3764162810ce4de5a7dc4d
zackenichi <thekenichi26@gmail.com>
2023-09-28 16:01:19 +0800

    setup material ui and theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container } from '@mui/material';
+import { FC } from 'react';
+import { CoinFlip } from './components/CoinFlip';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" data-testid="app-container">
+      <Box
+        sx={{
+          flexGrow: 1,
+          alignItems: 'center',
+          flexDirection: 'column',
+          p: 4,
+        }}
+      >
+        <CoinFlip />
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/CoinFlip/CoinFlip.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const CoinFlip: FC = () => {
+  return <div>CoinFlip</div>;
+};
+
+export { CoinFlip };
